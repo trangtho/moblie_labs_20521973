b371f4232fd40479fb94964fa0bb6ed4eed85205
trangtho <nguyenthitrangtho2512@gmail.com>
2023-05-31 15:20:11 +0700

    commit Lab5

--- app/src/main/java/com/example/lab05/MainActivity.java
+++ app/src/main/java/com/example/lab05/MainActivity.java
@@ -3,12 +3,123 @@ package com.example.lab05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnFadeInXml, btnFadeInCode, btnFadeOutXml, btnFadeOutCode, btnBlinkXml,
+            btnBlinkCode, btnZoomInXml, btnZoomInCode, btnZoomOutXml, btnZoomOutCode, btnRotateXml,
+            btnRotateCode, btnMoveXml, btnMoveCode, btnSlideUpXml, btnSlideUpCode, btnBounceXml,
+            btnBounceCode, btnCombineXml, btnCombineCode;
+    private ImageView ivUitLogo;
+    private Animation.AnimationListener animationListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewsByIds();
+        initVariables();
+
+//        final Animation animation = AnimationUtils.loadAnimation(MainActivity.this,
+//                R.anim.anim_fade_in);
+//
+//        animation.setAnimationListener(animationListener);
+//
+//        btnFadeInXml.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ivUitLogo.startAnimation(animation);
+//            }
+//        });
+
+        handleClickAnimationXml(btnFadeInXml, R.anim.anim_fade_in);
+        handleClickAnimationXml(btnFadeInCode, R.anim.anim_fade_in);
+        handleClickAnimationXml(btnFadeOutXml, R.anim.anim_fade_out);
+        handleClickAnimationXml(btnFadeOutCode, R.anim.anim_fade_out);
+        handleClickAnimationXml(btnBlinkXml, R.anim.anim_blink);
+        handleClickAnimationXml(btnBlinkCode, R.anim.anim_blink);
+        handleClickAnimationXml(btnZoomInXml, R.anim.anim_zoom_in);
+        handleClickAnimationXml(btnZoomInCode, R.anim.anim_zoom_in);
+        handleClickAnimationXml(btnZoomOutXml, R.anim.anim_zoom_out);
+        handleClickAnimationXml(btnZoomOutCode, R.anim.anim_zoom_out);
+
+        handleClickAnimationXml(btnRotateXml, R.anim.anim_rotate);
+        handleClickAnimationXml(btnMoveXml, R.anim.anim_move);
+        handleClickAnimationXml(btnSlideUpXml, R.anim.anim_slide_up);
+        handleClickAnimationXml(btnBounceXml, R.anim.anim_bounce);
+        handleClickAnimationXml(btnCombineXml, R.anim.anim_combine);
+
     }
+
+    private void findViewsByIds() {
+        ivUitLogo = (ImageView) findViewById(R.id.iv_uit_logo);
+        btnFadeInXml = (Button) findViewById(R.id.btn_fade_in_xml);
+        btnFadeInCode = (Button) findViewById(R.id.btn_fade_in_code);
+        btnFadeOutXml = (Button) findViewById(R.id.btn_fade_out_xml);
+        btnFadeOutCode = (Button) findViewById(R.id.btn_fade_out_code);
+        btnBlinkXml = (Button) findViewById(R.id.btn_blink_xml);
+        btnBlinkCode = (Button) findViewById(R.id.btn_blink_code);
+        btnZoomInXml = (Button) findViewById(R.id.btn_zoom_in_xml);
+        btnZoomInCode = (Button) findViewById(R.id.btn_zoom_in_code);
+        btnZoomOutXml = (Button) findViewById(R.id.btn_zoom_out_xml);
+        btnZoomOutCode = (Button) findViewById(R.id.btn_zoom_out_code);
+        btnRotateXml = (Button) findViewById(R.id.btn_rotate_xml);
+        btnRotateCode = (Button) findViewById(R.id.btn_rotate_code);
+        btnMoveXml = (Button) findViewById(R.id.btn_move_xml);
+        btnMoveCode = (Button) findViewById(R.id.btn_move_code);
+        btnSlideUpXml = (Button) findViewById(R.id.btn_slide_up_xml);
+        btnSlideUpCode = (Button) findViewById(R.id.btn_slide_up_code);
+        btnBounceXml = (Button) findViewById(R.id.btn_bounce_xml);
+        btnBounceCode = (Button) findViewById(R.id.btn_bounce_code);
+        btnCombineXml = (Button) findViewById(R.id.btn_combine_xml);
+        btnCombineCode = (Button) findViewById(R.id.btn_combine_code);
+    }
+
+    private void initVariables() {
+        animationListener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Toast.makeText(getApplicationContext(), "Animation Stopped",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        };
+    }
+
+    private void handleClickAnimationXml(Button btn, int animId)
+    {
+        // Sinh viên tự sửa lại code cho phù hợp
+        final Animation animation = AnimationUtils.loadAnimation(MainActivity.this,
+                animId);
+
+        animation.setAnimationListener(animationListener);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ivUitLogo.startAnimation(animation);
+            }
+        });
+    }
+
+
 }
